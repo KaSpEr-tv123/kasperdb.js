8f5201d2ab52a121849553024a4ff1fdeaa13521
ᎧᎮ ᭄乛ｋａｓｐｅｒｅｎｏｋ᭄ᴮᴼˢ <103499954+KaSpEr-tv123@users.noreply.github.com>
2023-12-13 00:37:23 +0200

    Added `on` function, for event handling

--- README.md
+++ README.md
@@ -31,6 +31,30 @@ console.log(data.money.count);
 const data = db.getData("money");
 console.log(data.count);
 ```
+
+## Event Handling with `on` Function
+### Listen for Data Retrieval Event
+```javascript
+db.on("getData", (eventData) => {
+  console.log(`Data retrieved from key ${eventData.key} in file ${eventData.filename}`);
+});
+```
+Attributes in `eventData` for event `getData`:
+- key
+- data
+- filename
+
+### Listen for Data Save Event
+```javascript
+db.on("saveData", (eventData) => {
+  console.log(`Data saved with key ${eventData.key} in file ${eventData.filename}`);
+});
+```
+Attributes in `eventData` for event `saveData`:
+- key
+- value
+- filename
+
 ## Dependencies
 - [fs](https://nodejs.org/api/fs.html): File system module.
 - [msgpack-lite](https://www.npmjs.com/package/msgpack-lite): MessagePack implementation for Node.js.
--- kasperdb.js
+++ kasperdb.js
@@ -3,7 +3,11 @@ const msgpack = require('msgpack-lite');
 
 class DB {
   constructor(args) {
-    this.filename = args.filename + args.extension;
+    if (args.extension) {
+      this.filename = args.filename + args.extension;
+    } else {
+      this.filename = args.filename + ".db"
+    }
     
     if (args.data) {
       const binaryData = msgpack.encode(args.data);
@@ -11,19 +15,39 @@ class DB {
     }
   }
 
-  saveData(kay, value) {
+  on(event, func) {
+    if (event == "getData") this.onGetData = func;
+    if (event == "saveData") this.onSaveData = func;
+  }
+
+  saveData(key, value) {
     let data = this.getData();
-    data[kay] = value;
+    data[key] = value;
     const binaryData = msgpack.encode(data);
     fs.writeFileSync(this.filename, binaryData);
+    if (this.onSaveData) this.onSaveData({
+        key: key,
+        value: value,
+        filename: this.filename
+      });
   }
 
   getData(key=null) {
     const readBinaryData = fs.readFileSync(this.filename);
     const decodedObject = msgpack.decode(readBinaryData);
     if (key != null) {
+      if (this.onGetData) this.onGetData({
+        key: key,
+        data: decodedObject[key],
+        filename: this.filename
+      });
       return decodedObject[key]
     } else {
+      if (this.onGetData) this.onGetData({
+        key: "[null]",
+        data: decodedObject,
+        filename: this.filename
+      });
       return decodedObject
     }
   }
